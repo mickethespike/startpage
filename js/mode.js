22da4ac4e12f232c91639eefeb00956d50ad228b
michael johansson <micke@mickejohansson.info>
2018-06-21 16:38:44 +0200

    made more intuitive you're welcome felix

--- js/mode.js
+++ js/mode.js
@@ -14,8 +14,8 @@ $(() => {
 function mode() {
   // if light mode == true
   if ($('#toggle-mode').prop('checked')) {
-    $('link[href="./css/dark.css"]').attr('href','./css/light.css');
-  } else {
     $('link[href="./css/light.css"]').attr('href','./css/dark.css');
+  } else {
+    $('link[href="./css/dark.css"]').attr('href','./css/light.css');
   }
 }
