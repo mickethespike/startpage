a4d02597faff27ff2b55dd813c4a892fceb1f57d
michael johansson <micke@mickejohansson.info>
2018-06-24 11:44:16 +0200

    Added card remover

--- js/add_card.js
+++ js/add_card.js
@@ -1,4 +1,5 @@
 let cards = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : []; //Check if there's an item called data, if it does'n't exist then it'll return an empty array
+let card_amount = 0;
 
 const add_cards = () => {
 	let data = {};
@@ -8,9 +9,15 @@ const add_cards = () => {
 	data['button_label'] = document.getElementById('button_label').value;
 
   let html = `
-  <div class="col-sm-6">
+  <div class="col-sm-6" id="${data['card_title'].replace('.', '_').replace(' ', '_')}">
 		<div class="card">
 			<div class="card-body">
+				<div class="dropdown">
+						<button type="button" class="dropdown-toggle card-settings fa fa-ellipsis-h" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+						<div class="dropdown-menu">
+						<a class="dropdown-item" href="#" onclick="delete_card(${data['card_title'].replace('.', '_').replace(' ', '_')})">Delete</a>
+					</div>
+				</div>
 				<h5 class="card-title">${data['card_title']}</h5>
 				<p class="card-text">${data['description']}</p>
 				<a href="${data['url']}" class="btn btn-primary">${data['button_label']}</a>
@@ -32,23 +39,37 @@ store_array('data', cards);
 const load_data = () => {
   let data = localStorage.getItem('data') ? JSON.parse(localStorage.getItem('data')) : []; //here I set data to the saved array, if there wasn't saved data then it'll return an empty array
   //console.log(Boolean(localStorage.getItem('data')));
-  for(let i = 0; i < data.length;i++) { //loop into that array
+  for(let i = 0; i < data.length; i++) { //loop into that array
     let keys = Object.keys(data[i]); //Not important but would help you if you added more info
 
-	let html = `
-  <div class="col-sm-6">
-		<div class="card">
-			<div class="card-body">
-				<h5 class="card-title">${data[i]['card_title']}</h5>
-				<p class="card-text">${data[i]['description']}</p>
-				<a href="${data[i]['url']}" class="btn btn-primary">${data[i]['button_label']}</a>
+		let html = `
+	  <div class="col-sm-6" id="${data[i]['card_title'].replace('.', '_').replace(' ', '_')}">
+			<div class="card">
+				<div class="card-body">
+					<div class="dropdown">
+						<button type="button" class="dropdown-toggle card-settings fa fa-ellipsis-h" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+						<div class="dropdown-menu">
+							<a class="dropdown-item" href="#" onclick="delete_card(${data[i]['card_title'].replace('.', '_').replace(' ', '_')})">Delete</a>
+						</div>
+					</div>
+					<h5 class="card-title">${data[i]['card_title']}</h5>
+					<p class="card-text">${data[i]['description']}</p>
+					<a href="${data[i]['url']}" class="btn btn-primary">${data[i]['button_label']}</a>
+				</div>
 			</div>
-		</div>
-	</div>`;
-  document.getElementsByClassName('row')[1].innerHTML += html;
-  }
-	//let handler = {'cards_title':'','description':'','url':'','button_label':''};
+		</div>`;
+	  document.getElementsByClassName('row')[1].innerHTML += html;
 
+		card_amount += 1;
+  }
 }
 
-load_data();
+const delete_card = (card_id) => {
+	document.getElementById(card_id.id).remove(0);
+}
+
+$(document).ready(() => {
+	load_data();
+});
+
+console.log(card_amount);
