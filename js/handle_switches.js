25ebe059f015723f4b087344c349293f0b475cc4
Michal Piatkowski <michal@kampinas.se>
2018-09-18 09:02:38 +0200

    Divided up colors into light and dark theme Fixed handle_switches to swap things faster/more seaminglessly

--- js/handle_switches.js
+++ js/handle_switches.js
@@ -1,29 +1,25 @@
 'use strict';
 
-const colorStyleLink = document.querySelector('link[href="./css/light.css"]');
-
+const colorStyleLink = document.querySelector('link[href="./css/dark.css"]');
 const useDarkThemeTag = 'useDarkTheme';
-const themeSlider = document.getElementById('theme-input');
-const themeDesc = document.getElementById('theme-state-desc');
-
 const showDonationButtonTag = 'showDonationButton';
-const donationWidget = document.getElementById('coffee-btn');
-const donationSlider = document.getElementById('donation-input');
-const donationDesc = document.getElementById('donation-state-desc');
 
 function setTheme(isDark) {
-    if (isDark) {
-        colorStyleLink.setAttribute('href', './css/dark.css');
-        themeDesc.innerHTML = "Dark";
-    } else {
-        colorStyleLink.setAttribute('href', './css/light.css');
-        themeDesc.innerHTML = "Light";
-    }
+    const themeSlider = document.getElementById('theme-input');
+    const themeDesc = document.getElementById('theme-state-desc');
+    setThemeLink(isDark);
+    themeDesc.innerHTML = isDark ? 'Dark' : 'Light';
     themeSlider.checked = isDark;
     localStorage.setItem(useDarkThemeTag, isDark);
 }
 
+function setThemeLink(isDark) {
+    colorStyleLink.setAttribute('href', isDark ? './css/dark.css' : './css/light.css');
+}
+
 function setDonationVisible(isVisible) {
+    const donationWidget = document.getElementById('coffee-btn');
+    const donationDesc = document.getElementById('donation-state-desc');
     if (isVisible) {
         donationWidget.style.display = 'block';
         donationDesc.innerHTML = "Show";
@@ -31,7 +27,7 @@ function setDonationVisible(isVisible) {
         donationWidget.style.display = 'none';
         donationDesc.innerHTML = "Hide";
     }
-    donationSlider.checked = isVisible;
+    document.getElementById('donation-input').checked = isVisible;
     localStorage.setItem(showDonationButtonTag, isVisible);
 }
 
@@ -40,11 +36,17 @@ function getLocalBool(key) {
 }
 
 if (localStorage.getItem(useDarkThemeTag) != '') {
-    setTheme(getLocalBool(useDarkThemeTag));
+    setThemeLink(getLocalBool(useDarkThemeTag));
 }
 
-if (localStorage.getItem(showDonationButtonTag) != '') {
-    setDonationVisible(getLocalBool(showDonationButtonTag));
-} else {
-    setDonationVisible(true);
-}
+window.onload = () => {
+    if (localStorage.getItem(useDarkThemeTag) != '') {
+        setTheme(getLocalBool(useDarkThemeTag));
+    }
+
+    if (localStorage.getItem(showDonationButtonTag) != '') {
+        setDonationVisible(getLocalBool(showDonationButtonTag));
+    } else {
+        setDonationVisible(true);
+    }
+};
